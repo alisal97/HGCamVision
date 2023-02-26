c87ab28e61cc18034667df210a58e39a42253dd4
Ali Sal <68872543+alisal97@users.noreply.github.com>
2023-02-26 17:46:50 +0100

    confidence tweaks

--- HandPoseCamera/CameraViewController.swift
+++ HandPoseCamera/CameraViewController.swift
@@ -421,7 +421,7 @@ extension CameraViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
     private func processPoints(thumbTipPoint: VNRecognizedPoint, indexTipPoint: VNRecognizedPoint, littleDIPPoint: VNRecognizedPoint, ringDIPPoint: VNRecognizedPoint, middleDIPPoint: VNRecognizedPoint) {
         
         // Ignore low confidence points.
-        guard thumbTipPoint.confidence > 0.91 && indexTipPoint.confidence > 0.85 && littleDIPPoint.confidence > 0.81 && ringDIPPoint.confidence > 0.81 && middleDIPPoint.confidence > 0.83
+        guard thumbTipPoint.confidence > 0.87 && indexTipPoint.confidence > 0.85 && littleDIPPoint.confidence > 0.81 && ringDIPPoint.confidence > 0.81 && middleDIPPoint.confidence > 0.83
         else {
             return
         }
